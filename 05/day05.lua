b9299e23209c220084c735fa124d71e2ae4a8d8a
Thomas A. Werne <werneta@gmail.com>
2022-12-04 23:41:19 -0800

    Add some comments to day 5's solutions
    Now playing:
    Raw, Unfiltered Calamity by DM DOKURO

--- 05/day05.lua
+++ 05/day05.lua
@@ -12,22 +12,27 @@ function day05(fname, keep_order)
         row_done = false
 
         if (row == "") then
+            -- Switching from parsing the initial stackup to the move commands
             stackup = false
-        end
-
-        if stackup then
+        elseif stackup then
+            -- Group the text row into sets of 4 characters
             for i = 1, #row, 4 do
                 stack = (i + 4) // 4
-                crate = row:sub(i + 1, i + 1)
                 if stacks[stack] == nil then
+                    -- Make sure we have a stack in place
                     table.insert(stacks, stack, {})
                 end
-                if row:sub(i, i) == "[" and crate ~= " " then
+                if row:sub(i, i) == "[" then
+                    -- Got a crate, push it to the end of the correct stack
+                    crate = row:sub(i + 1, i + 1)
                     table.insert(stacks[stack], 1, crate)
                 end
             end
-        elseif row ~= "" then
-            _, _, num, from, to = string.find(row, "move (%d+) from (%d+) to (%d+)")
+        else
+            -- Lua captures are not terrible
+            _, _, num, from, to = string.find(row,
+                "move (%d+) from (%d+) to (%d+)")
+            -- ... sure would be nice if I got numbers when I asked for numbers
             from = stacks[tonumber(from)]
             to = stacks[tonumber(to)]
 
@@ -38,6 +43,8 @@ function day05(fname, keep_order)
                 -- Part 2
                 from_idx = #from - num + 1
             end
+            -- Remove values from the right location in the source and append to
+            -- the destination
             for i = 1, num do
                 table.insert(to, #to + 1, table.remove(from, from_idx))
             end
