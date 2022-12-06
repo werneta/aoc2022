7c591b7c1a0b01215a340a65ab461e6cc09f389b
Thomas A. Werne <werneta@gmail.com>
2022-12-05 22:05:18 -0800

    Refactor day 6 solution
    Now playing:
    Infinity Chasers by Fractal Dreamers

---
+++ 06/day06.lua
@@ -0,0 +1,32 @@
+#!/usr/bin/env lua
+
+function day06(len)
+    fname = arg[1]
+
+    file = assert(io.open(fname, "r"))
+
+    stream = file:read("*line")
+
+    for n = 1, (#stream - len) do
+        word = stream:sub(n, n + len - 1)
+        match = false
+        for i = 1, len do
+            c = word:sub(i, i)
+            for j = i + 1, len do
+                if c == word:sub(j, j) then
+                    match = true
+                    break
+                end
+            end
+            if match then
+                break
+            end
+        end
+        if not match then
+            print(n + len - 1)
+            break
+        end
+    end
+
+    file:close()
+end
